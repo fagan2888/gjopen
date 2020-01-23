c8dd6bb315d016ec43e7388a07589cfa80e5e3be
JP <jp@jpatrickpark.com>
2020-01-23 12:55:00 -0500

    initiate repository

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # gjopen
 Making predictions for Good Judgment® Open
+
+## Prerequisites
+
+* Python (3.6)
+* MxNet
+* gluonts (https://github.com/awslabs/gluon-ts)
---
+++ utilities/read_files.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Callbacks implementation. Inspired by Keras.
+"""
+
+import pandas as pd
+import os
+
+def read_noaa_txt(file_path):
+    """Read a txt file downloaded from noaa.gov
+    TODO: Ideally create no intermediate file"""
+    pass
+    
+
+def download_and_read_noaa_txt(url, data_dir):
+    """Download and read a txt file from noaa.gov 
+    e.g. ftp://aftp.cmdl.noaa.gov/products/trends/co2/co2_trend_gl.txt"""
+    pass
